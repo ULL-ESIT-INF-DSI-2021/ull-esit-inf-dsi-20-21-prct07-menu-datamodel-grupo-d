eedca977e2bc58cddac2364782fdea511b1e493e
Yeixon Morales Gonzalez <alu0101133324@ull.edu.es>
2021-04-04 19:48:50 +0100

    Modificando Tabuladores

--- src/ingredient.ts
+++ src/ingredient.ts
@@ -17,39 +17,39 @@
  */
 
 export class Ingredient {
-    constructor(private readonly name: string, 
-                private readonly country: string,
-                private readonly city: string,
-                private readonly category: string,
-                private readonly kcal: number,
-                private readonly macro: [string, number][],
-                private readonly price_kg: number) {}
+  constructor(private readonly name: string, 
+              private readonly country: string,
+              private readonly city: string,
+              private readonly category: string,
+              private readonly kcal: number,
+              private readonly macro: [string, number][],
+              private readonly price_kg: number) {}
 
-    getName(): string {
-      return this.name;
-    }
+  getName(): string {
+    return this.name;
+  }
 
-    getCountry(): string {
-      return this.country;
-    }
+  getCountry(): string {
+    return this.country;
+  }
 
-    getCity(): string {
-      return this.city;
-    }
+  getCity(): string {
+    return this.city;
+  }
 
-    getCategory(): string {
-      return this.category;
-    }
+  getCategory(): string {
+    return this.category;
+  }
 
-    getCalories(): number {
-      return this.kcal;
-    }
+  getCalories(): number {
+    return this.kcal;
+  }
 
-    getMacros(): [string, number][] {
-      return this.macro;
-    }
+  getMacros(): [string, number][] {
+    return this.macro;
+  }
 
-    getPrice(): number {
-      return this.price_kg;
-    }
+  getPrice(): number {
+    return this.price_kg;
+  }
 }
