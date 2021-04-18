3f1b59de32c6a86220fb964c2b1121ab57a9a566
eric <eric.durr.20@ull.edu.es>
2021-04-18 18:12:02 +0100

    added dish doc

--- src/dish.ts
+++ src/dish.ts
@@ -1,5 +1,50 @@
+
+
+import {Ingredient, FoodCategory, Macro} from './ingredient'
+
+export type DishCategory = "Starter" | "First" | "Main" | "Dessert";
+
 /**
- * Class Dish
+ * # Class Dish
+ * Esta clase se encarga de aunar los distintos ingredientes que componen un plato y de agregar
+ * algunas propiedades al plato que serán de utilidad para la lógica de creación de menús.
+ * 
+ * Se asume como paso previo en la interfaz final que la creación de los platos permitirá
+ * establecer una metodología de evaluación de aceptación de los menús. Cómo ejemplo de ello
+ * se incluye la categoría del alimento, que será un dato descriptivo muy útil a la hora de 
+ * confirmar la validez de un menú. 
+ * 
+ * La clase, además de contener los atributos esperados:
+ * 
+ * - Nombre del plato
+ * - categoría del plato (mediante un tipo de dato personalizado, tal y como se ha hecho con anterioridad)
+ * - lista de ingredientes que lo componen
+ * 
+ * también es capaz de evaluar las calorías totales del plato así como su precio en base a los ingredientes 
+ * y otros datos de interés, para ello se definen los métodos: 
+ * 
+ * - totalDishCalories() - devuelve un valor numérico que representa las Kcal totales del plato 
+ *    Uso:
+ *    ```TS
+ *    let mashedPotatoesCalories: number = mashedPotatoes.totalDishCalories()
+ *    ``` 
+ * 
+ * - totalDishPrice() - devuelve un valor numérico que representa el coste del plato 
+ *    Uso:
+ *    ```TS
+ *    let mashedPotatoesPrice: number = mashedPotatoes.totalDishPrice()
+ *    ``` 
+ * 
+ * - showIngredients() - devuelve una cadena de caracteres que expone los ingredientes y su cantidad en gramos 
+ *    Uso:
+ *    ```TS
+ *    console.log(mashedPotatoes.showIngredients()) // ">Potatoes (200g) Milk (100g) Butter (50g) Salt (25g) "
+ *    ``` 
+ * - getDishMacro() - devuelve una lista de macronuetrientes que contiene el plato 
+ * 
+ * - dominantIngredientTypeOnDish() - devuelve el tipo de ingrediente más presente en el plato
+ * 
+ *   
  * @param name Name of the dish
  * @param category Category of the dish
  * @param ingredeints Ingredients in the dish
@@ -9,12 +54,6 @@
  * @method getDishMacro() Returns the total amount of proteins, carbs, fats and fiber in the dish
  * @method dominantIngredientType() Calcualtes the most dominant type of ingredient of the dish 
  */
-
-
-import {Ingredient, FoodCategory, Macro} from './ingredient'
-
-export type DishCategory = "Starter" | "First" | "Main" | "Dessert";
-
 export class Dish {
   constructor(readonly name: string,
               readonly category: DishCategory,
